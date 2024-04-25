332497777ed855551ee0c9144000507755893c98
TALITA ANTUNES <127903101+Talitaav@users.noreply.github.com>
2024-04-25 19:23:37 -0300

    Update Exercicio03.java

--- tarefa12/Exercicio03.java
+++ tarefa12/Exercicio03.java
@@ -3,6 +3,9 @@ package tarefa12MetodosEmJava;
 import java.util.Scanner;
 
 public class Exercicio03 {
+	 /*3) Verificar Palíndromo: Desenvolva um método que receba uma string como
+	 entrada e retorne verdadeiro se ela for um palíndromo (ou seja, se ela pode
+	 ser lida da mesma forma de trás para frente), e falso caso contrário.*/
 
 	public static boolean palindromo(String str) {
 
@@ -25,9 +28,7 @@ public class Exercicio03 {
 	}
 
 	public static void main(String[] args) {
-		// 3) Verificar Palíndromo: Desenvolva um método que receba uma string como
-		// entrada e retorne verdadeiro se ela for um palíndromo (ou seja, se ela pode
-		// ser lida da mesma forma de trás para frente), e falso caso contrário.
+		
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Digite uma frases: ");
